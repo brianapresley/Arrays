ad9892d0bb201867bf5a066bbf85adbf5c5da2a2
Briana Presley <presley.briana@gmail.com>
2019-07-24 16:34:39 -0400

    Added index with frames

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -3,7 +3,26 @@
 namespace ArraysProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] frames = new int[10];
+
+            frames[0] = 20;
+            frames[1] = 27;
+            frames[2] = 5;
+            frames[3] = 11;
+            frames[4] = 15;
+            frames[5] = 8;
+            frames[6] = 18;
+            frames[7] = 29;
+            frames[8] = 6;
+            frames[9] = 22;
+
+            int total = 0;
+            int index = 0;
+            while (index < 10) {
+                total = total + frames[index];
+                index = index + 1;
+            }
+            Console.WriteLine($"Total is {total}");
         }
     }
 }
